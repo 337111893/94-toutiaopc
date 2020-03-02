5cdd92af93ff2b46f301695eeff6b170e92fface
yutianzhengchong <337111893@qq.com>
2020-03-03 00:53:09 +0800

    页面分析-新建登录和主页

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '@/views/home'
 // import Home from '../views/Home.vue'
-
+import Login from '@/views/login'
 Vue.use(VueRouter)
 
 const routes = [
@@ -14,6 +14,11 @@ const routes = [
     path: '/Home',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
   }
   // {
   //   // 按需加载
