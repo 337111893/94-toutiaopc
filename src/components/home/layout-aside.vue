81598a0b6c3f51b8dad04cdf328b08c6f749d2a4
yutianzhengchong <337111893@qq.com>
2020-03-03 22:27:51 +0800

    主页模块-左侧导航配置路由

--- src/components/home/layout-aside.vue
+++ src/components/home/layout-aside.vue
@@ -5,38 +5,38 @@
         <img src="../../assets/img/logo_admin.png" alt="">
       </div>
       <!-- 导航菜单 -->
-      <el-menu background-color ="#353b4e" text-color="#adafb5">
+      <el-menu router background-color ="#353b4e" text-color="#adafb5">
           <!-- 首页 1级-->
-          <el-menu-item>
+          <el-menu-item index='/home'>
               <i class="el-icon-thumb"></i>
               <span>首页</span>
           </el-menu-item>
           <!-- 内容管理 -->
-          <el-submenu>
+          <el-submenu index="2">
                 <template slot="title">
                         <i class="el-icon-menu"></i>
                         <span>内容管理</span>
                 </template>
                         <!-- <template slot="title">分组一</template> -->
-                        <el-menu-item >发布文章</el-menu-item>
-                        <el-menu-item >内容列表</el-menu-item>
-                        <el-menu-item >评论列表</el-menu-item>
-                        <el-menu-item >素材管理</el-menu-item>
+                        <el-menu-item index='/home/publish'>发布文章</el-menu-item>
+                        <el-menu-item index='/home/articles'>内容列表</el-menu-item>
+                        <el-menu-item index='/home/comment'>评论列表</el-menu-item>
+                        <el-menu-item index='/home/material'>素材管理</el-menu-item>
           </el-submenu>
           <!-- 粉丝管理 -->
-          <el-submenu>
+          <el-submenu index="3">
                 <template slot="title">
                         <i class="el-icon-s-grid"></i>
                         <span>粉丝管理</span>
                 </template>
                         <!-- <template slot="title">分组一</template> -->
-                        <el-menu-item >图文数据</el-menu-item>
-                        <el-menu-item >粉丝概况</el-menu-item>
-                        <el-menu-item >粉丝画像</el-menu-item>
-                        <el-menu-item >粉丝列表</el-menu-item>
+                        <el-menu-item index='/home/picture'>图文数据</el-menu-item>
+                        <el-menu-item index='/home/fansinfo'>粉丝概况</el-menu-item>
+                        <el-menu-item index='/home/fansphoto'>粉丝画像</el-menu-item>
+                        <el-menu-item index='/home/fanslist'>粉丝列表</el-menu-item>
           </el-submenu>
           <!-- 账户信息 -->
-          <el-menu-item>
+          <el-menu-item index='/home/account'>
               <i class="el-icon-user-solid"></i>
               <span>账户信息</span>
           </el-menu-item>
