78f925cbf51c93d469820febcf1592b143f8c904
yutianzhengchong <337111893@qq.com>
2020-03-05 22:24:02 +0800

    请求拦截器

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import '@/permission' // 引入权限模块
 import ElementUI from 'element-ui'// 引入UI
-import axios from 'axios' // 引入axios
+import axios from '@/utils/request' // 引入封装的request.js
 import Components from '@/components' // 引入axios
 import 'element-ui/lib/theme-chalk/index.css'// 引入样式
 import '@/styles/index.less'
@@ -11,7 +11,6 @@ Vue.config.productionTip = false
 Vue.use(Components) // 全局注册自己开发的组件，就是components下的index.js
 Vue.use(ElementUI)// 全局注册element-ui框架，一旦注册都可以使用
 // 配置axios的baseURL
-axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0' // 配置公共的请求头地址
 Vue.prototype.$axios = axios // 将axios赋值给 Vue对象的原型属性
 new Vue({
   router,
--- src/views/home/second_home.vue
+++ src/views/home/second_home.vue
@@ -4,7 +4,7 @@
     <!-- 幻灯片 -->
     <el-carousel :interval="4000" type="card" height="300px">
       <el-carousel-item v-for="item in list" :key="item">
-         <img :src="item" alt="">
+        <img :src="item" alt=""/>
       </el-carousel-item>
     </el-carousel>
   </div>
@@ -15,10 +15,11 @@ export default {
   data () {
     return {
       list: [
+        'http://img2.imgtn.bdimg.com/it/u=2785932284,2599924006&fm=26&gp=0.jpg',
+        'http://img0.imgtn.bdimg.com/it/u=3143086120,207801152&fm=26&gp=0.jpg',
         'http://img5.imgtn.bdimg.com/it/u=3388941488,2735432575&fm=26&gp=0.jpg',
-        'http://img1.imgtn.bdimg.com/it/u=2554214457,1269801370&fm=11&gp=0.jpg',
-        'http://img2.imgtn.bdimg.com/it/u=1784773965,1273693181&fm=26&gp=0.jpg',
-        'http://img4.imgtn.bdimg.com/it/u=3819462408,3448612178&fm=26&gp=0.jpg']
+        'http://img5.imgtn.bdimg.com/it/u=1522119016,74832853&fm=26&gp=0.jpg'
+      ]
     }
   }
 }
@@ -29,6 +30,6 @@ export default {
   /* calc 计算的意思 */
   height: calc(100vh - 60px);
   background-size: cover;
-  background-image: url("http://img1.imgtn.bdimg.com/it/u=2294080717,3624105796&fm=26&gp=0.jpg");
+  background-image: url("http://img2.imgtn.bdimg.com/it/u=2785932284,2599924006&fm=26&gp=0.jpg");
 }
 </style>
