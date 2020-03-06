2bee8363d4fff9ead6505147bbc238c613c5d8fe
yutianzhengchong <337111893@qq.com>
2020-03-06 12:05:37 +0800

    打开和关闭评论

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -24,7 +24,7 @@
           <template slot-scope="obj">
             <el-button size="small" type='text'>修改</el-button>
              <!-- 文本内容要根据 当前行里面的评论状态决定显示还是隐藏 -->
-            <el-button  size="small" type='text'>{{ obj.row.comment_status ? '关闭' : '打开'}}评论</el-button>
+            <el-button  @click="openOrClose(obj.row)" size="small" type='text'>{{ obj.row.comment_status ? '关闭' : '打开'}}评论</el-button>
             </template>
        </el-table-column>
     </el-table>
@@ -68,6 +68,34 @@ export default {
       // index 代表当前的索引
       // 该函数需要返回一个值 用来显示
       return cellValue ? '正常' : '关闭'
+    },
+    //  打开或者关闭逻辑
+    openOrClose (row) {
+      const mess = row.comment_status ? '关闭' : '打开'
+      // $confirm 也支持 promise 点击确定会进入到then 点击取消会进入到catch
+      this.$confirm(`是否确定${mess}评论`, '提示').then(() => {
+        // 调用打开或者关闭接口
+        this.$axios({
+          url: '/comments/status', // 请求地址
+          method: 'put', // 请求类型
+          // query参数
+          params: {
+            article_id: row.id // 要求参数的文章id
+          },
+          data: {
+            //  body参数
+            allow_comment: !row.comment_status // 是打开还是关闭  此状态和评论状态相反
+          }
+        }).then(() => {
+          //   成功了 提示个消息 然后 重新拉取数据4
+          this.$message.success(`${mess}评论成功`)
+          //  重新拉取数据
+          this.getComment() // 调用重新拉取数据的方法
+        }).catch(() => {
+          //   表示失败了 会进入到catch
+          this.$message.error(`${mess}评论失败`)
+        })
+      })
     }
   },
   created () {
